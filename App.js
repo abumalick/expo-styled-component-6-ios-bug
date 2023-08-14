23d3b3d1c783c0849649d09a07e7e2990f005b92
abumalick <abdullah.hilson@gmail.com>
2023-08-14 10:29:16 +0200

    feat: add styled components and reproduction bug

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
 
+const Wrapper = styled.View`
+  background-color: red;
+  border-color: red;
+  border-width: 3px;
+`;
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Wrapper
+        style={{
+          backgroundColor: "blue", // Overrides
+          borderColor: "blue", // Does not overrides!
+        }}
+      >
+        <Text>Open up App.js to start working on your app!</Text>
+      </Wrapper>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
